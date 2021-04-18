6e31af2dee03ccd5e0d478aeef6baada2ef57b01
Student User <student@workstation.lab.example.com>
2021-04-17 22:24:48 -0400

    ada covid coy

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,4 +2,5 @@
 print "Asky is here.\n";
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "KENAPA NEW LINE DI FILE INI TIDAK BERFUNGSI????\n";
+print "\nTAmbah baris baru yang berisi Informasi rahasia tentang COVID";
 ?>
